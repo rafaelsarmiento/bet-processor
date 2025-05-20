66b168217e5825418d4221a4cfb15350fb54eb4a
Rafael Sarmiento <rafaelsarmientoromero@gmail.com>
2025-05-20 14:40:22 +0200

    Add initial project structure

---
+++ src/test/java/com/sportradar/bet_processor/BetProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sportradar.bet_processor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
